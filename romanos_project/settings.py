52e38f2aa2408c3f0019b2032fc9fbf678a2495e
Ocrezzu <davidjrf95@gmail.com>
2020-10-31 02:09:10 -0500

    cambiw

--- romanos_project/settings.py
+++ romanos_project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitennoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
 ]
 
 ROOT_URLCONF = 'romanos_project.urls'
