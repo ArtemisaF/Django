3cef7ea3020fa9e01e17b7906176e10c74f6d2f0
Ocrezzu <davidjrf95@gmail.com>
2020-10-31 01:40:46 -0500

    cambio2

--- romanos_project/settings.py
+++ romanos_project/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =(
     os.path.join(BASE_DIR,'static'),
 )
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
